d1f7d8bef7e767f07bf60b5d20a31ff3143e507e
ZZZar007 <harukiat505@gmail.com>
2026-07-18 15:12:54 +0700

    Third Commit

---
+++ Assets/TutorialInfo/Scripts/test/Ball.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+public enum BallColor
+{
+    White,
+    Red,
+    Green,
+    Yellow,
+    Brown,
+    Blue,
+    Black,
+    Pink
+}
+
+public class Ball : MonoBehaviour
+
+{
+    [SerializeField]
+    private int point;
+
+    [SerializeField]
+    private BallColor color;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/TutorialInfo/Scripts/test/test.cs
@@ -0,0 +1,28 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private int n = 0;
+    private float timer = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        n++;
+        Debug.Log(Time.deltaTime);
+
+
+        if (timer >= 1f)
+        {
+            Debug.Log(n);
+            timer = 0f;
+            n++;
+        }
+    }
+}
