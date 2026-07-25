fa02ff01d070b7758bcf8ea013956f683162fd07
haruki-athome <harukiat505@gmail.com>
2026-07-25 15:53:34 +0700

    Fifth Commit

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -21,7 +21,16 @@ public class Ball : MonoBehaviour, IPointerClickHandler
     [SerializeField]
     private BallColor color;
 
-    public void OnPointerClick(PointerEventData eventData)
+    [SerializeField]
+    private MeshRenderer rd;
+
+void Awake()
+	{
+		rd = GetComponent<MeshRenderer>();
+	}
+
+
+	public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(point);
         GameManager.instance.PlayerScor += point;
@@ -38,5 +47,44 @@ public class Ball : MonoBehaviour, IPointerClickHandler
     void Update()
     {
 
+    }
+    public void SetColorandPoint(BallColor color)
+    {
+
+        switch (color)
+        {
+            case BallColor.White:
+            point = 0;
+				rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+            point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Green:
+            point = 2;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Yellow:
+            point = 3;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Brown:
+            point = 4;
+                rd.material.color = Color.brown;
+                break;
+            case BallColor.Blue:
+            point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColor.Black:
+            point = 6;
+                rd.material.color = Color.black;
+                break;
+            case BallColor.Pink:
+            point = 7;
+                rd.material.color = Color.pink;
+                break;
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,17 +6,38 @@ public class GameManager : MonoBehaviour
     private int playerScore;
     public int PlayerScor { get { return playerScore; } set { playerScore = value;}  }
 
+    [SerializeField]
+    private GameObject[] ballPositions;
+
+    [SerializeField]
+    private GameObject ballPrefab;
+
     public static GameManager instance;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        instance = this;
-    }
+        SetBall(BallColor.White, 0);
+		SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Green, 2);
+        SetBall(BallColor.Yellow, 3);
+		SetBall(BallColor.Brown, 4);
+		SetBall(BallColor.Blue, 5);
+		SetBall(BallColor.Black, 6);
+		SetBall(BallColor.Pink, 7);
+	}
 
     // Update is called once per frame
     void Update()
     {
         
     }
+    private void SetBall(BallColor color,int i)
+    {
+    GameObject ball = Instantiate(ballPrefab,
+                      ballPositions[i].transform.position, 
+                      Quaternion.identity);
+    Ball b = ball.GetComponent<Ball>();
+    b.SetColorandPoint(color);
+	}
 }
